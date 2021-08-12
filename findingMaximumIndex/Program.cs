6e31a43524448b7c544c6c27695b27340b36ddc1
Andrey Rasskazov <ranzansan@gmail.com>
2021-08-12 18:21:46 +0300

    Добавьте файлы проекта.

---
+++ findingMaximumIndex/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace findingMaximumIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()); // сколько чисел будут вводиться на следующей строке
+            string s = Console.ReadLine(); // сохранить все введенную строку
+            string[] ss = s.Split(' '); // разделить введенную строку по пробелам и сохранить в массив строкового типа
+
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int number = Convert.ToInt32(ss[i]);
+                numbers[i] = number;
+            }
+
+            int max = numbers[0];
+            int indexMax = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                    indexMax = i;
+                }
+            }
+
+            Console.WriteLine(max);
+            Console.WriteLine(indexMax);
+            Console.ReadKey();
+        }
+    }
+}
